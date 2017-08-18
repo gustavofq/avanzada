cfd2d6f78466d90417be83fac03da2eb63e19495
Gustavo Quintana <31053651+gustavofq@users.noreply.github.com>
2017-08-18 08:41:42 -0300

    Update Avanzada.java
    probando pull

--- Avanzada/src/avanzada/Avanzada.java
+++ Avanzada/src/avanzada/Avanzada.java
@@ -16,7 +16,7 @@ public class Avanzada {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("facu se la come");
+        System.out.print("facu se la come a reee");
     }
     
 }
